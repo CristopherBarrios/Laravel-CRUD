e1c07ee444530f4b2e02b496b2b805eca72a6659
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-08 15:43:25 -0600

    TerminadaLaAPI

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\studentController;
 
 Route::get('/students', [studentController::class,'index']);
-    Route::get('/students/{id}', function () {
-    return 'Obteniendo un estudiante';
-    });
+    Route::get('/students/{id}', [studentController::class,'show']);
     Route::post('/students',  [studentController::class,'store']);
-    Route::put('/students/{id}', function () {
-    return 'Actualizando estudiante';
-    });
-    Route::delete('/students/{id}', function () {
-    return 'Eliminando estudiante';
-    });
+    Route::put('/students/{id}', [studentController::class,'update']);
+    Route::patch('/students/{id}', [studentController::class,'updatePartial']);
+    Route::delete('/students/{id}', [studentController::class,'destroy']);
